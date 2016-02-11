9618dfef28b13cec5c25ff68d991e58132dd125d
thaiduchyk <thaiduchyk@gmail.com>
2016-02-11 12:37:43 +0200

    Finish exercises 7.7

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,6 +10,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#<CSS id for error explanation>'
+    assert_select 'div.<CSS class for field with error>'
   end
 
   test "valid signup information" do
@@ -21,5 +23,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    #assert flash.empty?
+    assert_equal flash[:success], "Welcome to the Sample App!"
   end
 end
